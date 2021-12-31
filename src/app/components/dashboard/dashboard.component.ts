520f9c4d01cc18af90b3c3ec08ade3589c9849ff
Dhairya <dhairyashelar1@gmail+654+654165+4*/.com ~+*/>
2021-12-31 12:15:03 +0530

    UI Changes

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/services/api.service';
+import countries from '../../../assets/countries.json'
 
 @Component({
   selector: 'app-dashboard',
@@ -12,9 +13,16 @@ export class DashboardComponent implements OnInit {
   weatherForm!: FormGroup;
   weatherData: any = [];
   ferenheitTemp: any;
+  countries: Array<any> = [];
+  weather_icons: Array<any> = [];
+  errorMssge: boolean = false;
+
+  countryName: any;
+  country: any;
 
   public d = new Date();
   day: any;
+  today: any;
   todayDate: any;
   todayTime: any;
 
@@ -24,11 +32,30 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.prepareForm();
+    this.getDay();
 
-    this.day = this.d.getDay();
+    this.countries = countries;
     this.todayDate = this.d.getDate();
     this.todayTime = (this.d.getHours());
-    console.log("🚀 ~ file: dashboard.component.ts ~ line 30 ~ DashboardComponent ~ ngOnInit ~ this.todayTime", this.todayTime)
+  }
+  
+  getDay(){
+    this.day = this.d.getDay();
+    if(this.day === 1){
+      this.today = "Monday";
+    } else if(this.day === 2) {
+      this.today = "Tuesday";
+    } else if(this.day === 3) {
+      this.today = "Wednesday";
+    } else if(this.day === 4) {
+      this.today = "Thursday";
+    } else if(this.day === 5) {
+      this.today = "Friday";
+    } else if(this.day === 6) {
+      this.today = "Saturday";
+    } else {
+      this.today = "Sunday";
+    }
   }
 
   prepareForm(){
@@ -37,15 +64,25 @@ export class DashboardComponent implements OnInit {
     })
   }
 
+  selectedCountry(){
+    this.country = this.countryName.name;
+    console.log(this.countryName)
+ }
+
   getWeatherDetails(){
     const { countryName } = this.weatherForm.getRawValue();
+    console.log("🚀 ~ file: dashboard.component.ts ~ line 42 ~ DashboardComponent ~ getWeatherDetails ~ countryName", this.country)
 
-    this.apiService.getWeatherDetails(countryName).subscribe(response => {
+    this.apiService.getWeatherDetails(this.country).subscribe(response => {
       this.weatherData = response;
-      console.log("🚀 ~ file: dashboard.component.ts ~ line 34 ~ DashboardComponent ~ this.apiService.getWeatherDetails ~ this.weatherData", this.weatherData);
-      if(this.weatherData){
-        // (16°C × 9/5) + 32
-        this.ferenheitTemp = (((this.weatherData.current.temperature) * (9/5)) + 32);
+      console.log("🚀 ~ file: dashboard.component.ts ~ line 78 ~ DashboardComponent ~ this.apiService.getWeatherDetails ~ this.weatherData", this.weatherData)
+      if(this.weatherData.success === false){
+        this.errorMssge = true;
+      } else {
+        if(this.weatherData){
+          this.ferenheitTemp = Math.round((((this.weatherData.current.temperature) * (9/5)) + 32)*100)/100;
+          this.errorMssge = false;
+        }
       }
     })
   }
